86435a9e81025b51d8980ada785248cd665eb017
Tiago de Conto <tiagodc_89@yahoo.com.br>
2018-09-13 08:42:52 -0300

    slam bash bag

--- bash/slam_bash.sh
+++ bash/slam_bash.sh
@@ -4,15 +4,15 @@
 ## chmod u+x slam_bash
 
 #full path to the directory containing the .pcap files
-path="/home/tiago/Downloads/"
+path="/media/tiago/TDC/Foridar/TLS_2018/"
 
 #time (in seconds) to leave the ROS terminals open per file
-tempo=90
+tempo=480
 
 mkdir -p $path'pcd_temp'
 
 #loop over the *.pcap files in the input directory 
-for file in  $path*.pcap
+for file in  $path'clouds/'*.bag
 do
 
 	sleep 2
@@ -21,13 +21,18 @@ do
 	#open Rviz
 	timeout $tempo xterm -e "source ~/catkin_loam/devel/setup.bash && roslaunch loam_velodyne loam_velodyne.launch" &
 
-	#save .pcd files for all slam frames 
+	# save .pcd files for all slam frames 
 	timeout $tempo xterm -e "cd "$path"pcd_temp && source ~/catkin_loam/devel/setup.bash && rosrun pcl_ros pointcloud_to_pcd input:=/velodyne_cloud_registered" &
 
-	#process the .pcap file
-	timeout $tempo xterm -e 'source ~/catkin_loam/devel/setup.bash && sleep 1 && roslaunch velodyne_pointcloud VLP16_points.launch pcap:="'$file'" read_once:="true" max_range:="80" min_range:="1"' &
+	# #process the .pcap file	
+	timeout $tempo xterm -e 'source ~/catkin_loam/devel/setup.bash && sleep 2 && rosbag play -r 0.5 '$file &
 
 	#convert all pcd frames to a single .laz file
-	#sleep $tempo && xterm -e 'cd '$path' && ./pcd2laz -f pcd_temp -o '"${file%.pcap}.laz"' && cd pcd_temp && rm *.pcd'
+	sleep $tempo && xterm -e 'cd '$path' && ./pcd2laz -f pcd_temp -o '"${file%.bag}.laz"
+
+	rm $path'pcd_temp/'*.pcd
 
 done
+
+rm -r $path'pcd_temp/'
+cd $path'clouds/' && mv *.laz ../reprocessed/
