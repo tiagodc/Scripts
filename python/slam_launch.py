e08d25c1aa31ef80b630314d62c879662d41e60c
Tiago de Conto <tiagodc_89@yahoo.com.br>
2018-09-19 22:18:08 -0300

    quaternion recognition

--- python/slam_launch.py
+++ python/slam_launch.py
@@ -68,156 +68,165 @@ print('# defining global variables')
 
 
 # bag files path
-os.chdir(r'/home/tiago/Desktop/slam_tests')
+os.chdir(r'/media/tiago/OS/Work/gerdau')
 
-# variables used to convert imu_data and parse pointCloud2
-rBag = r'20180627_euc1_x_hor.bag'
-tops = [r'/velodyne_points', r'/ekf_quat', r'/imu_data']
-radius = None
-swap = 'xyz'
-
-# variables definig SLAM parameters
-hectorSlam = False
-useImu = True
-playRatio = 3
-recTopics = [r'/imu/data', r'/integrated_to_init', r'/velodyne_cloud_registered']
-sourcePath = r'~/catkin_loam'
-
-# variables used for generating the LAZ point cloud
-pcd2laz = '~/pcd2laz/bin/Release/pcd2laz' 
-pcdDir = 'pcd_temp'
-oLaz = re.sub(r'\.bag$', r'.laz', rBag)
-
-print('### processing: ' + rBag)
+# list all bag files
+bagFiles = []
+for i in os.listdir('.'):
+    if re.match(r'.+\.bag$', i) is not None:
+        bagFiles.append(i)
 
 
-#########################################
-print('# getting sensorMsg topics')
+for rBag in bagFiles:
+    # variables used to convert imu_data and parse pointCloud2
+    # rBag = r'20180627_euc1_x_hor.bag'
+    tops = [r'/velodyne_points', r'/ekf_quat', r'/imu_data']
+    radius = None
+    swap = 'xyz'
 
-wBag = re.sub(r'\.bag$', '_sensorMsg.bag', rBag)
-bag = rosbag.Bag(rBag)
+    # variables definig SLAM parameters
+    hectorSlam = False
+    useImu = True
+    playRatio = 0.25
+    recTopics = [r'/imu/data', r'/integrated_to_init', r'/velodyne_cloud_registered']
+    sourcePath = r'~/catkin_loam'
 
-writeBag = rosbag.Bag(wBag, 'w')
+    # variables used for generating the LAZ point cloud
+    pcd2laz = '~/pcd2laz/bin/Release/pcd2laz' 
+    pcdDir = 'pcd_temp'
+    oLaz = re.sub(r'\.bag$', r'.laz', rBag)
 
-for topic, msg, t in bag.read_messages(topics=tops):
-    if( topic == tops[2] ):
-        imu = msg
+    print('### processing: ' + rBag)
 
-    if(topic == tops[1]):
-        if(imu.time_stamp == msg.time_stamp):
-            imuMsg = convertImu(imu, msg)
-            writeBag.write('/imu/data', imuMsg, t)
-    
-    if(topic == tops[0]):
-        writeBag.write(topic, filterPointCloud2(msg, radius, swap), t)
 
-bag.close()
-writeBag.close()
+    #########################################
+    print('# getting sensorMsg topics')
 
-#########################################
-print('# performing SLAM')
+    wBag = re.sub(r'\.bag$', '_sensorMsg.bag', rBag)
+    bag = rosbag.Bag(rBag)
 
-launchPref = r'hector_' if hectorSlam else ''
-oBag = re.sub(r'(\.bag$)', launchPref + r'_slam.bag', wBag)
-rosKill = r'rosnode kill -a && killall -9 rosmaster'
+    writeBag = rosbag.Bag(wBag, 'w')
 
-bag = rosbag.Bag(wBag)
-bagTime = math.ceil(5 + (bag.get_end_time() - bag.get_start_time()) / playRatio)
-bag.close()
+    imu = None
+    for topic, msg, t in bag.read_messages(topics=tops):
+        if( topic == tops[2] ):
+            imu = msg
 
-cmdTimeout = r'timeout ' + str(int(bagTime)) + r' '
-cmdSleep = r'sleep 2 && '
-cmdStart = r'xterm -e "source ' + sourcePath + r'/devel/setup.bash && '
-cmdImu = r'' if useImu else r' --topics /velodyne_points'
+        if(topic == tops[1] and imu is not None):
+            if(imu.time_stamp == msg.time_stamp):
+                imuMsg = convertImu(imu, msg)
+                writeBag.write('/imu/data', imuMsg, t)
+            
+        if(topic == tops[0]):
+            writeBag.write(topic, filterPointCloud2(msg, radius, swap), t)
 
-roslaunch = cmdTimeout + cmdStart + r' roslaunch loam_velodyne ' + launchPref + r'loam_velodyne.launch" &'
-os.system(roslaunch)
+    bag.close()
+    writeBag.close()
 
-bagRecord = cmdSleep + cmdStart + r'rosbag record ' + r' '.join(recTopics) + r' -O ' + oBag + r'" &'
-os.system(bagRecord)
+    #########################################
+    print('# performing SLAM')
 
-bagPlay = cmdSleep + cmdStart + r'rosbag play ' + wBag + r' -r ' + str(playRatio) + r' ' + cmdImu + r'"'
-os.system(bagPlay)
+    launchPref = r'hector_' if hectorSlam else ''
+    oBag = re.sub(r'(\.bag$)', launchPref + r'_slam.bag', wBag)
+    rosKill = r'rosnode kill -a && killall -9 rosmaster'
 
-os.system(rosKill)
+    bag = rosbag.Bag(wBag)
+    bagTime = math.ceil(5 + (bag.get_end_time() - bag.get_start_time()) / playRatio)
+    bag.close()
 
-#########################################
-print('# writing LAZ point cloud')
+    cmdTimeout = r'timeout ' + str(int(bagTime)) + r' '
+    cmdSleep = r'sleep 2 && '
+    cmdStart = r'xterm -e "source ' + sourcePath + r'/devel/setup.bash && '
+    cmdImu = r'' if useImu else r' --topics /velodyne_points'
 
-if os.path.exists(pcdDir): 
+    roslaunch = cmdTimeout + cmdStart + r' roslaunch loam_velodyne ' + launchPref + r'loam_velodyne.launch" &'
+    os.system(roslaunch)
+
+    bagRecord = cmdSleep + cmdStart + r'rosbag record ' + r' '.join(recTopics) + r' -O ' + oBag + r'" &'
+    os.system(bagRecord)
+
+    bagPlay = cmdSleep + cmdStart + r'rosbag play ' + wBag + r' -r ' + str(playRatio) + r' ' + cmdImu + r'"'
+    os.system(bagPlay)
+
+    os.system(rosKill)
+
+    #########################################
+    print('# writing LAZ point cloud')
+
+    if os.path.exists(pcdDir): 
+        shutil.rmtree(pcdDir)
+
+    os.makedirs(pcdDir)
+
+    os.system('roscore &')
+
+    pclCmd = 'cd ' + pcdDir + ' && rosrun pcl_ros pointcloud_to_pcd input:=/velodyne_cloud_registered &'
+
+    os.system(pclCmd)
+
+    playCmd = 'rosbag play ' + oBag
+
+    os.system(playCmd)
+    os.system(rosKill)
+
+    lazCmd = pcd2laz + ' -f ' + pcdDir + ' -o ' + oLaz
+
+    os.system(lazCmd)
+
     shutil.rmtree(pcdDir)
 
-os.makedirs(pcdDir)
+    #########################################
+    print('# writing IMU and SLAM path`s information')
 
-os.system('roscore &')
+    bag = rosbag.Bag(oBag)
 
-pclCmd = 'cd ' + pcdDir + ' && rosrun pcl_ros pointcloud_to_pcd input:=/velodyne_cloud_registered &'
+    rad2deg = 180/math.pi
+    angs = []
+    slamPath = []
+    for topic, msg, t in bag.read_messages(topics=recTopics):
 
-os.system(pclCmd)
+        time = float(msg.header.stamp.secs) + float(msg.header.stamp.nsecs) / 10**9
 
-playCmd = 'rosbag play ' + oBag
+        if topic == recTopics[0]:
+            quat = (
+                msg.orientation.x,
+                msg.orientation.y,
+                msg.orientation.z,
+                msg.orientation.w
+            )
 
-os.system(playCmd)
-os.system(rosKill)
+            euler = tf.transformations.euler_from_quaternion(quat)
 
-lazCmd = pcd2laz + ' -f ' + pcdDir + ' -o ' + oLaz
+            # time, roll, pitch, yaw
+            info = [time, euler[0] * rad2deg, euler[1] * rad2deg, euler[2] * rad2deg]
+            angs.append(info)
 
-os.system(lazCmd)
+        if topic == recTopics[1]:
+            pose = msg.pose.pose
 
-shutil.rmtree(pcdDir)
+            quat = (
+                pose.orientation.x,
+                pose.orientation.y,
+                pose.orientation.z,
+                pose.orientation.w
+            )
 
-#########################################
-print('# writing IMU and SLAM path`s information')
+            euler = tf.transformations.euler_from_quaternion(quat)
+            
+            # time, x, y, z, roll, pitch, yaw
+            info = [time, pose.position.x, pose.position.y, pose.position.z, euler[0] * rad2deg, euler[1] * rad2deg, euler[2] * rad2deg]
+            slamPath.append(info)
 
-bag = rosbag.Bag(oBag)
+    bag.close()
 
-rad2deg = 180/math.pi
-angs = []
-slamPath = []
-for topic, msg, t in bag.read_messages(topics=recTopics):
+    if len(angs) > 0:
+        angs = numpy.array(angs)
+        oTxt = re.sub(r'\.bag$', r'_imu.txt', rBag)
+        numpy.savetxt(oTxt, angs, fmt="%f")
 
-    time = float(msg.header.stamp.secs) + float(msg.header.stamp.nsecs) / 10**9
+    if len(slamPath) > 0:
+        slamPath = numpy.array(slamPath)
+        oTxt = re.sub(r'\.bag$', r'_slam_path.txt', rBag)
+        numpy.savetxt(oTxt, slamPath, fmt="%f")
 
-    if topic == recTopics[0]:
-        quat = (
-            msg.orientation.x,
-            msg.orientation.y,
-            msg.orientation.z,
-            msg.orientation.w
-        )
-
-        euler = tf.transformations.euler_from_quaternion(quat)
-
-        # time, roll, pitch, yaw
-        info = [time, euler[0] * rad2deg, euler[1] * rad2deg, euler[2] * rad2deg]
-        angs.append(info)
-
-    if topic == recTopics[1]:
-        pose = msg.pose.pose
-
-        quat = (
-            pose.orientation.x,
-            pose.orientation.y,
-            pose.orientation.z,
-            pose.orientation.w
-        )
-
-        euler = tf.transformations.euler_from_quaternion(quat)
-        
-        # time, x, y, z, roll, pitch, yaw
-        info = [time, pose.position.x, pose.position.y, pose.position.z, euler[0] * rad2deg, euler[1] * rad2deg, euler[2] * rad2deg]
-        slamPath.append(info)
-
-bag.close()
-
-if len(angs) > 0:
-    angs = numpy.array(angs)
-    oTxt = re.sub(r'\.bag$', r'_imu.txt', rBag)
-    numpy.savetxt(oTxt, angs, fmt="%f")
-
-if len(slamPath) > 0:
-    slamPath = numpy.array(slamPath)
-    oTxt = re.sub(r'\.bag$', r'_slam_path.txt', rBag)
-    numpy.savetxt(oTxt, slamPath, fmt="%f")
-
-print('# done')
+    print('# done')
