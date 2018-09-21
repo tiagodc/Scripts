486894db7ba9dbfa365cef312a23a23fc474fadc
Tiago de Conto <tiagodc_89@yahoo.com.br>
2018-09-21 12:13:42 -0300

    tls correction auto loop

--- R/TLScorrection.R
+++ R/TLScorrection.R
@@ -3,7 +3,7 @@ require(rgl)
 require(magrittr)
 source('TLSprocess.R')
 
-# setwd('C:/Work/gerdau/')
+setwd('/media/tiago/OS/Work/gerdau/')
 
 swapAxes = function(las, swap='xyz'){
   
@@ -44,46 +44,47 @@ cloudSample = function(las, size=500000){
 
 lasFiles = dir(pattern = '\\.laz$')
 
-i = 7
-file = lasFiles[i] %T>% print
-slamFile = sub('\\.laz', '_slam_path.txt', file) %T>% print
-
-las = readLAS(file)
-slam = read.table(slamFile)
-
-if(grepl('00\\.laz$',file)){
-  # 00
-  swap = 'xzy'
-  las %<>% swapAxes(swap) ; slam %<>% swapAxes(swap)
+for(i in 1:10){
+  file = lasFiles[i] %T>% print
+  slamFile = sub('_sensorMsg_slam\\.laz', '_slam_path.txt', file) %T>% print
   
-} else if(grepl('45\\.laz$',file)){
-  # 45
-  las@data$Z = -las@data$Z ; slam[,4] = -slam[,4]
+  las = readLAS(file)
+  slam = read.table(slamFile)
+  
+  file %<>% sub(pattern = '_sensorMsg_slam', replacement = '', fixed = T)
+  if(grepl('00\\.laz$',file)){
+    # 00
+    swap = 'xzy'
+    las %<>% swapAxes(swap) ; slam %<>% swapAxes(swap)
+    
+  } else if(grepl('45\\.laz$',file)){
+    # 45
+    las@data$Z = -las@data$Z ; slam[,4] = -slam[,4]
+  }
+  
+  # apply(las@data[,1:3], 2, range)
+  
+  # shortLas = cloudSample(las)
+  # plot(shortLas) ; axes3d(col='#FFFFFF')
+  # spheres3d(slam[,2], slam[,3], slam[,4], 1, col='white')
+  
+  tempFile = sub('\\.laz$', '_temp.laz', file)
+  writeLAS(las, tempFile)
+  
+  rotationData = rotateCloud(tempFile, '~/LAStools/bin/')
+  lasRot = rotationData$cloud  
+  slam[,2:4] = as.matrix(slam[,2:4]) %*% rotationData$matrix
+  
+  # apply(lasRot@data[,1:3], 2, range)
+  # shortLas = cloudSample(lasRot)
+  # plot(shortLas) ; axes3d(col='#FFFFFF')
+  # spheres3d(slam[,2], slam[,3], slam[,4], 1, col='white')
+  
+  unlink(tempFile, force = T)
+  
+  rotFile = sub('\\.laz$', '_rot.laz', file)
+  writeLAS(lasRot, paste0('LAZ/',rotFile))
+  
+  slamFile = sub('\\.txt$', '_rot.txt', slamFile)
+  write.table(slam, paste0('LAZ/',slamFile), row.names = F, col.names = F)
 }
-
-apply(las@data[,1:3], 2, range)
-
-shortLas = cloudSample(las)
-plot(shortLas) ; axes3d(col='#FFFFFF')
-spheres3d(slam[,2], slam[,3], slam[,4], 1, col='white')
-
-tempFile = sub('\\.laz$', '_temp.laz', file)
-writeLAS(las, tempFile)
-
-rotationData = rotateCloud(tempFile)
-lasRot = rotationData$cloud  
-slam[,2:4] = as.matrix(slam[,2:4]) %*% rotationData$matrix
-
-apply(lasRot@data[,1:3], 2, range)
-shortLas = cloudSample(lasRot)
-plot(shortLas) ; axes3d(col='#FFFFFF')
-spheres3d(slam[,2], slam[,3], slam[,4], 1, col='white')
-
-unlink(tempFile, force = T)
-
-rotFile = sub('\\.laz$', '_rot.laz', file)
-writeLAS(lasRot, paste0('rotated/',rotFile))
-
-slamFile = sub('\\.txt$', '_rot.txt', slamFile)
-write.table(slam, paste0('rotated/',slamFile), row.names = F, col.names = F)
-
