d2533b7724bdf7e4e91001ad0b7ee765c0675b21
tiagodc <tiagodc_89@yahoo.com.br>
2020-04-22 04:56:21 -0300

    parallel web scrapping ubuntu

--- R/web_scrapping.R
+++ R/web_scrapping.R
@@ -1,4 +1,14 @@
+setwd('~/Desktop/')
 require(rvest)
+# require(foreach)
+# require(doParallel)
+
+start_url = as.double(commandArgs(T)[1])
+end_url   = start_url + as.double(commandArgs(T)[2])
+start_url = start_url + 1
+
+write_file = commandArgs(T)[3]
+log_file = commandArgs(T)[4]
 
 padZeros = function(number, digits=7){
     nstr = as.character(number)
@@ -13,8 +23,8 @@ webMiner = function(page_index, base_url = "https://www.wikiaves.com.br/"){
   
   session = html_session(url)
   raw_txt = read_html(url) %>% as.character()
-  
-  is_empty = grepl('<div class="m-alert__text">.*Mídia não encontrada.*?</div>', raw_txt)
+
+  is_empty = grepl('.*Mídia não encontrada.*', raw_txt)
   is_sound = grepl('id="imgSonograma"', raw_txt)
   
   if(is_empty){
@@ -39,38 +49,56 @@ webMiner = function(page_index, base_url = "https://www.wikiaves.com.br/"){
   
   img = sub('.+id=\"wa-foto\" class=\"img-responsive\" src=\"(.+?)">.+', '\\1', raw_txt)
   
-  species_info_url = base_url %>% paste0( html_node(session, 'a.wa-menu-especie') %>% html_attr('href') )
-  
-  species_imgs_url =  base_url %>% paste0( sub('.+<a href="(.+)\" class.+Fotos da Espécie.+', '\\1', raw_txt) )
-  
+  if(grepl('NÃO-IDENTIFICADA', raw_txt)){
+    species_info_url = 'NA'
+    species_imgs_url = 'NA'
+  }else{
+    species_info_url = base_url %>% paste0( html_node(session, 'a.wa-menu-especie') %>% html_attr('href') )
+    species_imgs_url = base_url %>% paste0( sub('.+<a href="(.+)\" class.+Fotos da Espécie.+', '\\1', raw_txt) )
+  }
+
   data = c(img = img, info = info, code = code, cite = cite, likes = likes, species_info = species_info_url, species_imgs = species_imgs_url)
   return(data)
 }
 
-write_file = 'enquia.txt'
-write(c('img', 'info', 'code', 'cite', 'likes', 'species_info', 'species_imgs'), 
-      write_file, ncolumns = 7, sep = '|', append = F)
-
-log_file = 'log.txt'
-write('errors', log_file, append = F)
-
 empty_counter = 0
 t0 = Sys.time()
 
-for(i in 0:9999999){
+# nCores = 4
+# cl = makePSOCKcluster(nCores)
+# registerDoParallel(cl)
+
+for(i in start_url:end_url) {
+# foreach(i = 0:9999999, .combine = 'c', .packages = 'rvest') %dopar% {
+
   paste('\nmining page:', padZeros(i), '\n') %>% cat
   print(Sys.time() - t0)
   
   data = tryCatch(webMiner(i), error=function() NULL)
   
   if(is.null(data)){
-    write(padZeros(i) , log_file, append = T)
+    paste('\n', Sys.time(), '- error trying to load page:', padZeros(i)) %>% write(log_file, append = T)
     paste('error trying to load page:', padZeros(i)) %>% message()
+  }else if(data == -1 || data == 0){
+    if(data[1] == -1) empty_counter = empty_counter + 1
+    next
+  }else{
+    sanity = sapply(data, stringr::str_length) %>% max
+    if(sanity > 500){
+      paste('\n', Sys.time(), '- HTML warning:', padZeros(i)) %>% write(log_file, append = T)
+    }else{
+      paste('\n', Sys.time(), '- mining page:', padZeros(i)) %>% write(log_file, append = T)
+    }
   }
   
+  empty_counter = 0
   empty_counter = ifelse(data[1] == -1, empty_counter + 1, 0) %>% as.double
   
-  if(empty_counter > 1000) break else if(data == -1 || data == 0) next
+  if(empty_counter > 1000) break 
   
-  write(data, write_file, 7, T, '|')    
+  paste0('\n', data) %>% write(write_file, 7, T, '|')    
 }
+
+# stopImplicitCluster()
+# stopCluster(cl)
+# registerDoSEQ()
---
+++ R/ws_caller.R
@@ -0,0 +1,16 @@
+setwd('~/Desktop')
+
+write_file = 'enquia.txt'
+write(c('img', 'info', 'code', 'cite', 'likes', 'species_info', 'species_imgs'),
+      write_file, ncolumns = 7, sep = '|', append = F)
+
+log_file = 'log.txt'
+write('status', log_file, append = F)
+
+setwd('~/Desktop/Projects/Scripts/R')
+
+n = 200000
+starts = seq(0, 5000000, n)
+for(i in starts){
+  paste('xterm -e "Rscript web_scrapping.R', i, n, write_file, log_file, '" &') %>% system
+}
